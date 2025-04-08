9012a8cde87c481e8c4ea6b57a4834aa25f89fa3
Nyéki Zsolt <nyekizsolt@gmail.com>
2025-04-08 21:34:40 +0200

    initial commit

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+
+import Places from "./Places.jsx";
+import ErrorPage from "./Error.jsx";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    setIsFetching(true);
+    async function fetchPlaces() {
+      try {
+        const response = await fetch("http://localhost:3000/placessss");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fecth places, please try again later.",
+        });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
+
+  if (error) {
+    return <ErrorPage title="An error occured!" message={error.message} />;
+  }
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
